98b28b309d2659a98d79850f3f8900b34588e46d
Thao Luu <thaoluu.310@gmail.com>
2019-10-29 23:11:36 +0700

    update file

--- selenium/webdriver_api/Topic_01_CheckEnvironment.java
+++ selenium/webdriver_api/Topic_01_CheckEnvironment.java
@@ -1,11 +1,8 @@
 package webdriver_api;
 
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,42 +13,29 @@ public class Topic_01_CheckEnvironment {
 	
 	@BeforeClass
 	public void beforeClass() {
-		//khoi tao trinh duyet firefox
 		driver = new FirefoxDriver();
-		
-		//cho element duoc hien thi truoc khi tuong tac trong vong 30s
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		//phong to trinh duyet
 		driver.manage().window().maximize();
-		
-		//mo trang web
 		driver.get("http://demo.guru99.com/v4/");
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	public void TC_01_() {
+
 	}
 
 	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	public void TC_02_() {
+		
 	}
 
 	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	public void TC_03_() {
+		
 	}
 
 	@AfterClass
 	public void afterClass() {
-		//tat trinh duyet
 		driver.quit();
 	}
 
